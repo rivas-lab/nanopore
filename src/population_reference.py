2590e7c678e52bedf079cc2dc99f074d7b00f3b3
Yosuke Tanigawa <ytanigaw@stanford.edu>
2017-03-12 19:37:27 -0700

    posterior done; haplotype_block.py

--- src/population_reference.py
+++ src/population_reference.py
@@ -12,7 +12,7 @@ import pgenlib as pg
 import misc
 from misc import Mismatch
 from misc import Nucleotide
-from haplotype import Haplotype
+#from haplotype import Haplotype
 
 class population_reference():
     def __init__(self, params):
@@ -32,14 +32,14 @@ class population_reference():
             self.load_pgen(chromosome)
         return(self.pgens[str(chromosome)])
     
-    def read_haplotype(self, chromosome, index_l, index_r):
-        pgen = self.get_pgen(chromosome)
-        sample_ct = pgen.get_raw_sample_ct()
-        alleles_list = np.zeros((index_r - index_l, 2 * sample_ct),
-                                dtype = np.int32)
-        pgen.read_alleles_range(index_l, index_r, alleles_list)
-        return([Haplotype(chromosome, index_l, index_r, alleles_list[:, i]) 
-                for i in range(alleles_list.shape[1])])
+#    def read_haplotype(self, chromosome, index_l, index_r):
+#        pgen = self.get_pgen(chromosome)
+#        sample_ct = pgen.get_raw_sample_ct()
+#        alleles_list = np.zeros((index_r - index_l, 2 * sample_ct),
+#                                dtype = np.int32)
+#        pgen.read_alleles_range(index_l, index_r, alleles_list)
+#        return([Haplotype(chromosome, index_l, index_r, alleles_list[:, i]) 
+#                for i in range(alleles_list.shape[1])])
 
     def read_alleles(self, chromosome, index_l, index_r):
         pgen = self.get_pgen(chromosome)
